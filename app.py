96f445aa7823c2263ae7ca7c749caff278d4c60c
saidalafghaniedsa <57323299+saiddddd@users.noreply.github.com>
2023-03-05 22:36:12 +0700

    app.py

--- app.py
+++ app.py
@@ -136,11 +136,10 @@ def main():
             st.dataframe(df.head())
             st.success("Your data has been uploaded successfully!")
             st.text("Statistic of your data is here...")
-            st.success("No. 1")
+            st.success("Your file contains {} rows and {} columns".format(df.shape[0), df.shape[1]))
             overview = overview_data(df, df.columns.values)
             overview
-            st.success("No. 2")
-            print(df.describe())
+
             
             status = st.radio("Please choose your detail: ", ["Sweetviz", "Pandas Profiling"])
             if status == "Pandas Profiling":
